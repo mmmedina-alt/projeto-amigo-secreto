d9f99f0f7ec401adfc5ca61ad3f6281267321a3c
mmmedina-alt <mmmedinaufcspa@gmail.com>
2025-03-18 23:51:40 -0300

    first commit

---
+++ challenge_amigo_secreto-main/src/js/app.js
@@ -0,0 +1,52 @@
+
+let amigos = [];
+
+function agregarAmigo() {
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.value.trim();
+
+    if (nombreAmigo === "") {
+        alert("Por favor, insira um nome");
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+
+    actualizarLista();
+
+    inputAmigo.value = "";
+    inputAmigo.focus();
+
+
+}
+
+function actualizarLista() {
+    const listaAmigoUl =document.getElementById("listaAmigos")
+    listaAmigoUl.innerHTML = "";
+
+    amigos.forEach(amigo => {
+        const li =document.createElement("li");
+        li.textContent = amigo;
+        listaAmigoUl.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Não há amigos para sortear. Adicione nomes primeiro..");
+        return
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    const amigoSorteado = amigos[indiceAleatorio]; 
+
+    const resultadoUl = document.getElementById("resultado");
+    resultadoUl.innerHTML = `<li>${amigoSorteado}</li>`;
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("btnAdicionar").addEventListener("click", agregarAmigo)
+    document.getElementById("btnSortear").addEventListener("click", sortearAmigo);
+});
